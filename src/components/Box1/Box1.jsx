d927612cc17f5f0ba3ae6b548a869ae92bda5069
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-09-04 23:34:00 +0530

    framer motion

---
+++ src/components/Box1/Box1.jsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+export default function Box1() {
+  const [animate, setAnimate] = useState(false);
+  return (
+    <div className="box-container">
+      <motion.div
+        initial={{
+          opacity: 0.1,
+        }}
+        animate={{
+          x: animate ? 1000 : 0,
+          //   scale: 1.5,
+          opacity: animate ? 1 : 0.4,
+          rotate: animate ? 360 : 0,
+        }}
+        transition={{
+          type: animate ? "spring" : "tween",
+          stiffness: animate ? 60 : 100, // to make faster type spring (duration : teen)
+          //   damping: 10, // to make slower type spring
+        }}
+        onClick={() => setAnimate(!animate)}
+        className="box"
+      ></motion.div>
+    </div>
+  );
+}
